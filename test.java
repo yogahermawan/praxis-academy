bbc95dfb1e880cc38cfa2a0073403a849c1e7efb
yogahermawan <yogahermawan43@gmail.com>
2019-08-19 14:38:50 +0700

    add folder novice

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test{
     public static void main(String [] args){
-        System.out.print("hello world");
+        String nama = "Yoga Hermawan";
+        System.out.println(nama);
+        System.out.println("hello world");
     }
 }
